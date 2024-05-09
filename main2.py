471637560d4c2bf8be4e427a280e7c4e9ce95484
yjdy <yuanjunfy@163.com>
2024-05-09 11:10:56 +0800

    规整代码

--- main2.py
+++ main2.py
@@ -73,131 +73,9 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from transformers import get_linear_schedule_with_warmup, AutoConfig
 from model.utils import create_adamw_optimizer
+from utils import CustomDataset,DataCollatorWithPadding,AddGaussianNoise,AddUniformNoise,list_files
 
 baseconfig = AutoConfig.from_pretrained(args.basepath)
-
-import random
-def list_files(path,shuffle=True):
-    datapath = []
-    for root, directories, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            datapath.append(file_path)
-    if shuffle:
-        random.shuffle(datapath)
-    return datapath
-
-
-class AddGaussianNoise:
-    def __init__(self, mean=0.0, std=0.0):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, data):
-        tensor = data["hidden_state_big"]
-        noise = torch.randn(tensor.size()) * self.std + self.mean
-        noisy_tensor = tensor + noise
-        data["hidden_state_big"] = noisy_tensor
-        return data
-
-
-class AddUniformNoise:
-    def __init__(self, std=0.0):
-        self.std = std
-
-    def __call__(self, data):
-        tensor = data["hidden_state_big"]
-        noise = (torch.rand_like(tensor) - 0.5) * self.std * 512 / tensor.shape[1]
-        noisy_tensor = tensor + noise
-        data["hidden_state_big"] = noisy_tensor
-        return data
-
-
-class CustomDataset(Dataset):
-    def __init__(self, datapath, transform=None):
-        self.data = datapath
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        # try:
-        data = torch.load(self.data[index])
-        new_data = {}
-        hidden_state = data['hidden_state'][:train_config["max_len"]][None, :]
-        input_ids = data['input_ids'][:train_config["max_len"]][None, :]
-        loss_mask = data["loss_mask"][:train_config["max_len"]][None, :]
-
-        # except:
-        #     with open("error_path.txt", "w") as file:
-        #         file.write(self.data[index])
-        #     print('error path',self.data[index])
-
-        length = hidden_state.shape[1]
-        # length_q = data['query_ids'].shape[1]
-        attention_mask = [1] * length
-        loss_mask = loss_mask[0].tolist()
-        loss_mask[-1] = 0
-
-        input_ids_target = input_ids[:, 1:]
-        zeropadding = torch.tensor([[0]])
-        input_ids_target = torch.cat((input_ids_target, zeropadding), dim=1)
-
-        target = hidden_state[:, 1:, :]
-        zeropadding = torch.zeros(1, 1, target.shape[2])
-        target = torch.cat((target, zeropadding), dim=1)
-        loss_mask[-1] = 0
-        new_data["attention_mask"] = attention_mask
-        new_data["loss_mask"] = loss_mask
-        new_data["target"] = target
-        new_data["hidden_state_big"] = hidden_state
-        new_data["input_ids"] = input_ids_target
-        # sample = torch.cat((data['xs'],data['xb']))
-        # sample=torch.cat((self.data[index]['x'],self.data[index]['logits']))
-        # label = data['y']
-
-        if self.transform:
-            new_data = self.transform(new_data)
-
-        return new_data
-
-
-class DataCollatorWithPadding:
-
-    def paddingtensor(self, intensors, N):
-        B, n, S = intensors.shape
-        # padding_tensor = torch.zeros(B, N - n, S,dtype=intensors.dtype)
-        padding_tensor = torch.zeros(B, N - n, S)
-        outtensors = torch.cat((intensors, padding_tensor), dim=1)
-        return outtensors
-
-    def paddingtensor2D(self, intensors, N):
-        B, n = intensors.shape
-        padding_tensor = torch.zeros(B, N - n, dtype=intensors.dtype)
-        outtensors = torch.cat((intensors, padding_tensor), dim=1)
-        return outtensors
-
-    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
-        max_length = max(item['hidden_state_big'].shape[1] for item in features)
-        batch_input_ids = torch.cat([self.paddingtensor2D(item['input_ids'], max_length) for item in features])
-        batch_hidden_states = torch.cat([self.paddingtensor(item['hidden_state_big'], max_length) for item in features])
-        batch_target = torch.cat([self.paddingtensor(item['target'], max_length) for item in features])
-        batch_loss_mask = torch.tensor(
-            [item['loss_mask'] + [0] * (max_length - len(item['loss_mask'])) for item in features])
-        batch_attention_mask = torch.tensor(
-            [item['attention_mask'] + [0] * (max_length - len(item['attention_mask'])) for item in features])
-        # batch_loss_mask = torch.ones_like(batch_loss_mask)
-        # batch_attention_mask=torch.ones_like(batch_attention_mask)
-        batch = {
-            "input_ids": batch_input_ids,
-            "hidden_states": batch_hidden_states,
-            "target": batch_target,
-            "attention_mask": batch_attention_mask,
-            "loss_mask": batch_loss_mask,
-        }
-        return batch
-
 
 def top_accuracy(output, target, topk=(1,)):
     # output.shape (bs, num_classes), target.shape (bs, )
@@ -286,8 +164,8 @@ with accelerator.main_process_first():
     # print('td',train_config["datapath"])
     # print(datapath)
     # exit()
-    traindataset = CustomDataset(traindatapath, transform=aug)
-    testdataset = CustomDataset(testdatapath)
+    traindataset = CustomDataset(traindatapath, transform=aug,max_len=train_config["max_len"])
+    testdataset = CustomDataset(testdatapath,max_len=train_config["max_len"])
     train_loader = DataLoader(traindataset, batch_size=train_config["bs"], shuffle=True, drop_last=True,
                               collate_fn=DataCollatorWithPadding(), num_workers=train_config["num_workers"],
                               pin_memory=True)
